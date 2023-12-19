778eb2df4bc8af9503969aa531931ed55b6ce551
Colin Heurteaux <colin.heurteaux@epitech.eu>
2023-12-19 21:25:03 +0100

    Added more error handling

--- compute_values.py
+++ compute_values.py
@@ -1,14 +1,35 @@
 import math
 
 def add_arithmetic_mean(values, new_value):
-    return (values["arithmetic_mean"] * (values["nb_elements"] - 1) + new_value) / values["nb_elements"]
+    if (new_value == 0):
+        print("104neutrinos: Value must not be equal to 0")
+        exit(84)
+    try:
+        return (values["arithmetic_mean"] * (values["nb_elements"] - 1) + new_value) / values["nb_elements"]
+    except:
+        print("104neutrinos: Cannot add value, implies division by zero")
+        exit(84)
 
 def add_harmonic_mean(values, new_value):
-    return (values["nb_elements"]) / (((values["nb_elements"] - 1) / values["harmonic_mean"]) + (1 / new_value))
+    if (new_value == 0):
+        print("104neutrinos: Value must not be equal to 0")
+        exit(84)
+    try:
+        return (values["nb_elements"]) / (((values["nb_elements"] - 1) / values["harmonic_mean"]) + (1 / new_value))
+    except:
+        print("104neutrinos: Cannot add value, implies division by zero")
+        exit(84)
 
 def add_standard_deviation(values, new_value):
+    if (new_value == 0):
+        print("104neutrinos: Value must not be equal to 0")
+        exit(84)
     top_value = (values["nb_elements"] - 1) * (values["standard_deviation"]**2 + (values["arithmetic_mean"] - values["previous_mean"])**2) + ((new_value - values["arithmetic_mean"])**2)
-    return math.sqrt(top_value / values["nb_elements"])
+    try:
+        return math.sqrt(top_value / values["nb_elements"])
+    except:
+        print("104neutrinos: Cannot add value, implies division by zero")
+        exit(84)
 
 def get_root_mean_square(values):
     return math.sqrt(values["arithmetic_mean"]**2 + values["standard_deviation"]**2)
